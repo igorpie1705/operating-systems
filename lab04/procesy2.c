22460f1ea61d07a944c8d1c06ce5007a7d77f40a
igorpie1705 <igorpie1705@gmail.com>
2025-03-27 11:23:56 +0100

    zrobiono zad2 lab04

--- lab04/procesy2.c
+++ lab04/procesy2.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/stat.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
@@ -9,19 +10,23 @@ int global = 0;
 int main(int argc, char *argv[]) {
   if (argc < 2) {
     fprintf(stderr, "Nie podano argumentu wejsciowego. \n");
-    return -1;
+    return EXIT_FAILURE;
   }
 
-  // TODO Obsluga bledu nieprawidlowego katalogu w arg wejsciowym.
+  struct stat sb;
+  if (stat(argv[1], &sb) == -1 || !S_ISDIR(sb.st_mode)) {
+    fprintf(stderr, "Podana sciezka nie jest prawidlowym katalogiem.\n");
+    return EXIT_FAILURE;
+  }
 
-  printf("Nazwa pliku: \n");
+  printf("Nazwa pliku: %s\n", argv[0]);
 
   pid_t child_pid = fork();
   int local = 0;
 
   if (child_pid < 0) {
     perror("Błąd fork()");
-    return 1;
+    return EXIT_FAILURE;
   }
   if (child_pid == 0) {
     printf("child process\n");
@@ -29,15 +34,24 @@ int main(int argc, char *argv[]) {
     local++;
     printf("child pid = %d, parent pid = %d\n", getpid(), getppid());
     printf("child's local = %d, child's global = %d\n", local, global);
-    int status = execl(argv[1], "ls", NULL);
+
+    execl("/bin/ls", "ls", argv[1], NULL);
+    perror("Blad execl()");
+    return EXIT_FAILURE;
+
   } else {
+    int status;
+    waitpid(child_pid, &status, 0);
+
     printf("parent process\n");
     printf("parent pid = %d, child pid = %d\n", getpid(), child_pid);
-    printf("Child exit code: %d\n"); // TODO dodac status
+    if (WIFEXITED(status)) {
+      printf("Child exit code: %d\n", WEXITSTATUS(status));
+    } else {
+      printf("Child did not exit normally.\n");
+    }
     printf("Parent's local = %d, parent's global = %d\n", local, global);
-    // TODO zwroc stosowny kod bledu
-  }
 
-  wait(NULL);
-  return 0;
+    return WEXITSTATUS(status);
+  }
 }
